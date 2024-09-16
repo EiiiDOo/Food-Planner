bb75395056343019f5e61ec57182dbd93cdccca7
EiiiDOo <ahmed.eid159753@gmail.com>
2024-09-16 23:37:13 +0300

    Unversioned Files

---
+++ app/src/main/java/com/example/foodplanner/Model/NoInternetDialog.java
@@ -0,0 +1,43 @@
+package com.example.foodplanner.Model;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import com.example.foodplanner.R;
+
+
+public class NoInternetDialog extends androidx.fragment.app.DialogFragment {
+
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return inflater.inflate(R.layout.test, container, false);
+//    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getDialog() != null && getDialog().getWindow() != null) {
+            getDialog().getWindow().setBackgroundDrawableResource(R.drawable.dialog_background);
+        }
+        Button btn =  view.findViewById(R.id.canc);
+
+        btn.setOnClickListener(v -> dismiss());
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.customdialognointernet, container, false);
+    }
+}
