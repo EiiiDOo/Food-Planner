a7630e843875d62ec15c615dc1b87eef7b626176
EiiiDOo <ahmed.eid159753@gmail.com>
2024-09-16 23:20:21 +0300

    add network listener

--- app/src/main/java/com/example/foodplanner/Details/Presenter/DetailsPresenterImpl.java
+++ app/src/main/java/com/example/foodplanner/Details/Presenter/DetailsPresenterImpl.java
@@ -3,35 +3,44 @@ package com.example.foodplanner.Details.Presenter;
 import android.util.Log;
 
 import com.example.foodplanner.Details.View.DetailsView;
+import com.example.foodplanner.MainActivity;
+import com.example.foodplanner.Model.DialogFragment;
 import com.example.foodplanner.Model.Ingredients;
 import com.example.foodplanner.Model.Meal;
 import com.example.foodplanner.Model.MealWithDay;
 import com.example.foodplanner.Model.Reposatery.ReposateryImpl;
 import com.example.foodplanner.Network.IngredientCallback;
 import com.example.foodplanner.Network.MealsCallBack;
+import com.example.foodplanner.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class DetailsPresenterImpl implements DetailsPresenterInterface, MealsCallBack, IngredientCallback {
-
+    private static DetailsPresenterImpl instance = null;
     ReposateryImpl reposatery;
     DetailsView detailsView;
-    List<Meal> listMeal = new ArrayList<>();
+    volatile List<Meal> listMeal = MainActivity.list;
     List<MealWithDay> listMealWithDay = new ArrayList<>();
+    CompositeDisposable compositeDisposable;
 
 
-    public List<MealWithDay> getListMealWithDay() {
-        return listMealWithDay;
-    }
-
-    public DetailsPresenterImpl(ReposateryImpl reposatery, DetailsView detailsView) {
+     public DetailsPresenterImpl(ReposateryImpl reposatery, DetailsView detailsView) {
         this.reposatery = reposatery;
         this.detailsView = detailsView;
-        getFavMeals();
+        compositeDisposable = new CompositeDisposable();
+
+
+    }
+    public static DetailsPresenterImpl getInstance(ReposateryImpl reposatery, DetailsView detailsView) {
+        if(instance == null) {
+            instance = new DetailsPresenterImpl(reposatery, detailsView);
+        }
+        return instance;
     }
 
     public List<Meal> getList() {
@@ -40,7 +49,6 @@ public class DetailsPresenterImpl implements DetailsPresenterInterface, MealsCal
 
     @Override
     public void onSuccessMeals(List<Meal> Meals) {
-
         detailsView.showMeal(Meals);
     }
 
@@ -52,59 +60,41 @@ public class DetailsPresenterImpl implements DetailsPresenterInterface, MealsCal
 
     @Override
     public void fetchMealsById(String id) {
-
-        reposatery.fetchMealsById(id).subscribeOn(Schedulers.io())
+        compositeDisposable.add(reposatery.fetchMealsById(id).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(e-> {
-
-                    detailsView.showMeal(e.getMeals());
-                }, e -> {
-
-                    detailsView.showErrorMsg(e.getMessage());
-                });
+                .subscribe(e-> detailsView.showMeal(e.getMeals()), e -> detailsView.showErrorMsg(e.getMessage())));
     }
 
     @Override
     public void invokeShowMealWithObj(Meal meal) {
-
         detailsView.showMealWithObj(meal);
     }
 
     @Override
     public void addTofav(Meal meal) {
-        reposatery.insertFavMeals(meal).subscribeOn(Schedulers.io())
+        compositeDisposable.add(reposatery.insertFavMeals(meal).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(() -> {
-                    detailsView.showSuccessMsg("Added Successfully");
-                }, e -> {
-                    detailsView.showErrorMsg(e.getMessage());
-                }).isDisposed();
+                .subscribe(() ->  detailsView.showSuccessMsg("Added Successfully")
+                , e -> detailsView.showErrorMsg(e.getMessage())));
     }
 
     @Override
     public void getFavMeals() {
-        reposatery.getFavMeals(reposatery.getFireBaseUser().getUid())
+        compositeDisposable.add(reposatery.getFavMeals(MainActivity.statUser)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(e -> {
-                            listMeal = e;
-                        }
-                        , e -> {
-                            detailsView.showErrorMsg(e.getMessage());
-                        });
+                .subscribe(e ->  listMeal = e , e -> detailsView.showErrorMsg(e.getMessage())));
 
     }
 
 
     public void addPlan(MealWithDay mealWithDay) {
-        reposatery.insertMealPlan(mealWithDay).subscribeOn(Schedulers.io())
+        compositeDisposable.add(reposatery.insertMealPlan(mealWithDay).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(() -> {
                     listMealWithDay.add(mealWithDay);
                     detailsView.showSuccessMsg("Added Successfully");
-                }, e -> {
-                    detailsView.showErrorMsg(e.getMessage());
-                });
+                }, e -> detailsView.showErrorMsg(e.getMessage())));
     }
 
 
@@ -117,27 +107,20 @@ public class DetailsPresenterImpl implements DetailsPresenterInterface, MealsCal
     @Override
     public void onSuccessIngredient(List<Ingredients> ingredients) {
 
-        reposatery.fetchMealsByIngredient(ingredients.get(0).getStrIngredient()).subscribeOn(Schedulers.io())
+        compositeDisposable.add(reposatery.fetchMealsByIngredient(ingredients.get(0).getStrIngredient()).subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(e -> {
-
-                    detailsView.showMeal(e.getMeals());
-                }, e -> {
-                    detailsView.showErrorMsg(e.getMessage());
-                });
+                .subscribe(e -> detailsView.showMeal(e.getMeals()), e -> detailsView.showErrorMsg(e.getMessage())));
     }
 
     @Override
     public void onFailureIngredient(String errorMsg) {
-
         detailsView.showErrorMsg(errorMsg);
     }
 
     @Override
     public boolean isFav(Meal meal) {
-        Log.d("DetailsFragment", "isFav: from is fav "+meal.getUserId());
-        Log.d("DetailsFragment", "isFav: from is fav "+listMeal.toString());
-        return listMeal.stream().anyMatch(e -> e.getIdMeal().equals(meal.getIdMeal()));
+        Log.d("TAG", "isFav: "+listMeal);
+        return listMeal.stream().anyMatch(e -> e.getStrMeal().equals(meal.getStrMeal()));
     }
 
     @Override
--- app/src/main/java/com/example/foodplanner/Details/View/DetailsFragment.java
+++ app/src/main/java/com/example/foodplanner/Details/View/DetailsFragment.java
@@ -1,15 +1,16 @@
 package com.example.foodplanner.Details.View;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.constraintlayout.widget.Group;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,46 +25,51 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.example.foodplanner.Authorization.AuthorizActivity;
 import com.example.foodplanner.Details.Presenter.DetailsPresenterImpl;
 import com.example.foodplanner.FireBase.FireBaseRemoteDatasourceImpl;
 import com.example.foodplanner.MainActivity;
 import com.example.foodplanner.Model.AllCountries;
 import com.example.foodplanner.Model.Country;
 import com.example.foodplanner.Model.DaysDialog;
+import com.example.foodplanner.Model.DialogFragment;
 import com.example.foodplanner.Model.Meal;
 import com.example.foodplanner.Model.MealWithDay;
 import com.example.foodplanner.Model.RepoRoom.Room.MealsfavLocalDataSourceImpl;
 import com.example.foodplanner.Model.Reposatery.ReposateryImpl;
 import com.example.foodplanner.Network.Base.RemoteDataSourceImpl;
+import com.example.foodplanner.Profile.View.ListenerSignin;
 import com.example.foodplanner.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
-public class DetailsFragment extends Fragment implements DetailsView {
+public class DetailsFragment extends Fragment implements DetailsView, ListenerSignin {
     final String TAG = "DetailsFragment";
-    boolean fav = true, calend = true;
     TextView nameOfMeal;
     DetailsPresenterImpl detailsPresenter;
-    ProgressBar progressBar;
     ImageButton addToFav, addToPlan;
     RecyclerView rvIngredients;
-    TextView instractions, category, backGroundProgressBar;
+    TextView instractions, category;
     IngredientAdapterdetails ingredientAdapter;
     ImageView image, imageCountry;
     String id;
     WebView wv;
     DaysDialog daysDialog;
     Meal mealFromLastfragment, mealReadyToSave;
+    ProgressBar progressBar;
+    Group group;
+    CompositeDisposable compositeDisposable;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        compositeDisposable = new CompositeDisposable();
 
-        Log.d(TAG, "onCreate: " + id);
     }
 
     @Override
@@ -77,11 +83,10 @@ public class DetailsFragment extends Fragment implements DetailsView {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         super.onViewCreated(view, savedInstanceState);
+        progressBar = view.findViewById(R.id.detailsProgressBar);
+        group = view.findViewById(R.id.detGroup);
         id = DetailsFragmentArgs.fromBundle(getArguments()).getID();
         mealFromLastfragment = DetailsFragmentArgs.fromBundle(getArguments()).getMealObj();
-
-        progressBar = view.findViewById(R.id.progressBar2);
-        backGroundProgressBar = view.findViewById(R.id.backProgrespar);
         nameOfMeal = view.findViewById(R.id.nameMeal);
         rvIngredients = view.findViewById(R.id.recyclerView);
         instractions = view.findViewById(R.id.instractions);
@@ -93,32 +98,30 @@ public class DetailsFragment extends Fragment implements DetailsView {
         rvIngredients.setAdapter(ingredientAdapter);
         wv = view.findViewById(R.id.webView);
         image = view.findViewById(R.id.imageDaily);
-        if(!(MainActivity.statUser.equals("guest"))){
-            detailsPresenter = new DetailsPresenterImpl(ReposateryImpl.getInstance(RemoteDataSourceImpl.getInstance(), FireBaseRemoteDatasourceImpl.getInstance(), MealsfavLocalDataSourceImpl.getInstance(this.getContext())), this);
+        detailsPresenter =  new DetailsPresenterImpl(ReposateryImpl.getInstance(RemoteDataSourceImpl.getInstance(), FireBaseRemoteDatasourceImpl.getInstance(), MealsfavLocalDataSourceImpl.getInstance(this.getContext())), this);
 
-            detailsPresenter.getFavMeals();
+//        if (!(MainActivity.statUser.equals("guest"))) {
+//            detailsPresenter.getFavMeals();
+//        }
+        if (mealFromLastfragment == null) {
+            detailsPresenter.fetchMealsById(id);
+        } else {
+            Log.d(TAG, "onViewCreated ELSE: " + mealFromLastfragment.getUserId());
+            detailsPresenter.invokeShowMealWithObj(mealFromLastfragment);
+        }
 
-            if (mealFromLastfragment == null){
-                detailsPresenter.fetchMealsById(id);
-            }
-            else {
-                Log.d(TAG, "onViewCreated ELSE: "+mealFromLastfragment.getUserId());
-                detailsPresenter.invokeShowMealWithObj(mealFromLastfragment);
-            }        }
 
     }
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     public void showMeal(List<Meal> meal) {
+        setImgIc(meal.get(0));
         mealReadyToSave = meal.get(0);
-        Log.d(TAG, "showMeal: "+mealReadyToSave.getStrArea());;
-        setImgCalend(mealReadyToSave.transferToMealWithDay(mealReadyToSave));
-        setImgIc(mealReadyToSave);
         hideProgressBar(mealReadyToSave);
+        setImgCalend(mealReadyToSave.transferToMealWithDay(mealReadyToSave));
         setData(mealReadyToSave);
         if (meal.get(0).getStrYoutube() != null && !meal.get(0).getStrYoutube().isEmpty()) {
-            wv.setVisibility(View.VISIBLE);
             wv.setWebViewClient(new WebViewClient());
             WebSettings webSettings = wv.getSettings();
             webSettings.setJavaScriptEnabled(true);
@@ -126,17 +129,19 @@ public class DetailsFragment extends Fragment implements DetailsView {
         } else
             wv.setVisibility(View.GONE);
     }
+
     @Override
     public void showMealWithObj(Meal meal) {
         mealReadyToSave = meal;
-
         hideProgressBar(meal);
         setData(meal);
+        setImgIc(mealReadyToSave);
 
     }
 
     @Override
     public void showErrorMsg(String error) {
+        progressBar.setVisibility(View.GONE);
         Toast.makeText(this.getContext(), error, Toast.LENGTH_SHORT).show();
     }
 
@@ -151,7 +156,6 @@ public class DetailsFragment extends Fragment implements DetailsView {
     }
 
 
-
     @Override
     public void showSuccessMsg(String msg) {
         Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
@@ -159,37 +163,46 @@ public class DetailsFragment extends Fragment implements DetailsView {
 
     void setImgIc(Meal meal) {
         if (detailsPresenter.isFav(meal)) {
-            Log.d(TAG, "setImgIc: in check is fav"+detailsPresenter.isFav(meal));
-            addToFav.setImageDrawable(getContext().getDrawable(R.drawable.favblack));
+            Log.d(TAG, "setImgIc: in check is fav" + detailsPresenter.isFav(meal));
+            addToFav.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.favblack));
             addToFav.setEnabled(false);
         } else {
-            addToFav.setImageDrawable(getContext().getDrawable(R.drawable.favwhite));
+            addToFav.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.favwhite));
             addToFav.setEnabled(true);
             addToFav.setOnClickListener(v -> {
-                meal.setUserId(detailsPresenter.getUserId());
-                detailsPresenter.addTofav(meal);
-                addToFav.setImageDrawable(getContext().getDrawable(R.drawable.favblack));
-                addToFav.setOnClickListener(null);});
+                if (MainActivity.statUser.equals("guest")) {
+                    DialogFragment dialogFragment = DialogFragment.newInstance(R.string.login, R.string.loginmessage
+                            , R.drawable.cheese, R.string.login, R.string.cancel, true, this);
+                    dialogFragment.show(getChildFragmentManager(), null);
+                } else {
+
+                    meal.setUserId(detailsPresenter.getUserId());
+                    detailsPresenter.addTofav(meal);
+                    addToFav.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.favblack));
+                    addToFav.setOnClickListener(null);
+                }
+            });
         }
     }
 
+    @SuppressLint("CheckResult")
     void setImgCalend(MealWithDay meal) {
-            addToPlan.setImageDrawable(getContext().getDrawable(R.drawable.addtocalend));
-            addToPlan.setOnClickListener(v -> {
+        addToPlan.setImageDrawable(AppCompatResources.getDrawable(requireContext(), R.drawable.addtocalend));
+        addToPlan.setOnClickListener(v -> {
+            if (MainActivity.statUser.equals("guest")) {
+                DialogFragment dialogFragment = DialogFragment.newInstance(R.string.login, R.string.loginmessage
+                        , R.drawable.bread, R.string.login, R.string.cancel, true, this);
+                dialogFragment.show(getChildFragmentManager(), null);
+            } else {
                 daysDialog = new DaysDialog(getActivity());
                 daysDialog.getConfirmButton().setOnClickListener(v1 -> {
                     daysDialog.getConfirmButton().setEnabled(false);
                     meal.setUserId(detailsPresenter.getUserId());
-                    daysDialog.getDays()
+                    compositeDisposable.add(daysDialog.getDays()
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe(day -> {
-//                                Log.d(TAG, "setImgCalend emit: "+day);
-//                                newMeal.setDay(day);
-//                                Log.d(TAG, "setImgCalend emit: "+newMeal.getDay());
-//                                detailsPresenter.addPlan(newMeal);
-//                                Log.d(TAG, "setImgCalend emit: after emitted");
-                                switch(day){
+                                switch (day) {
                                     case "1":
                                         MealWithDay newMeal = new MealWithDay(meal);
                                         newMeal.setDay(day);
@@ -226,23 +239,23 @@ public class DetailsFragment extends Fragment implements DetailsView {
                                         detailsPresenter.addPlan(newMeal7);
                                         break;
                                 }
-                            },e->{
-                                Toast.makeText(this.getContext(), "", Toast.LENGTH_SHORT).show();
-                            },()->{
+                            }, e -> Toast.makeText(this.getContext(), "", Toast.LENGTH_SHORT).show(), () -> {
                                 daysDialog.dismissDialog();
-                                Toast.makeText(this.getContext(), "", Toast.LENGTH_SHORT).show();});
+                                Toast.makeText(this.getContext(), "", Toast.LENGTH_SHORT).show();
+                            }));
                 });
-            });
-        }
+            }
+        });
+
+    }
 
 
     void hideProgressBar(Meal meal) {
-        new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            progressBar.setVisibility(View.INVISIBLE);
-            backGroundProgressBar.setVisibility(View.INVISIBLE);
-            setImgIc(meal);
-            setImgCalend(meal.transferToMealWithDay(meal));
-        }, 1500);
+        progressBar.setVisibility(View.GONE);
+        group.setVisibility(View.VISIBLE);
+//        setImgIc(meal);
+        setImgCalend(meal.transferToMealWithDay(meal));
+
     }
 
     void setData(Meal meal) {
@@ -250,9 +263,14 @@ public class DetailsFragment extends Fragment implements DetailsView {
         instractions.setText(meal.getStrInstructions());
         category.setText(meal.getStrCategory());
         imageCountry.setImageResource(getResourceId(meal.getStrArea()));
-        Glide.with(getContext()).load(meal.getStrMealThumb()).placeholder(R.drawable.foodplaceholder).into(image);
-        ingredientAdapter.updatedata(meal.getIngredients());
+        Glide.with(requireContext()).load(meal.getStrMealThumb()).placeholder(R.drawable.foodplaceholder).into(image);
+        ingredientAdapter.updatedata(meal.IngredientsReflect());
     }
 
 
+    @Override
+    public void okSignin() {
+        startActivity(new Intent(getActivity(), AuthorizActivity.class));
+        requireActivity().finish();
+    }
 }
